72f55e0757d2874175fa44053a72c4a215122a8b
nhduchuy2306 <nguyenhuy1323614@gmail.com>
2023-04-14 09:32:00 +0700

    update

--- src/main/java/com/notimplement/happygear/model/dto/CartItemDto.java
+++ src/main/java/com/notimplement/happygear/model/dto/CartItemDto.java
@@ -7,6 +7,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+/**
+A DTO (Data Transfer Object) representing a single item in a shopping cart.
+@Data
+@AllArgsConstructor
+@NoArgsConstructor public class CartItemDto {
+The ID of the product associated with this cart item.
+*/
 public class CartItemDto {
     private Integer productId;
     private Integer quantity;
