7f260a5286f2304ddf8c0a69243f99b060468e7d
nhduchuy <nh.duchuy2306@gmail.com>
2023-04-24 15:15:56 +0700

    update bashscript

--- build-staging-deploy.sh
+++ build-staging-deploy.sh
@@ -1,10 +1,8 @@
 
 # build spring boot application base on staging yml
+echo "Build spring boot application base on staging yml"
 mvn clean package -DskipTests -Pstag
 
-# go to target folder
-cd target
-
 # deploy to staging server
-scp -rf target admin@13.215.150.32:~/happygear
-
+echo "Deploy to staging server"
+scp -r target admin@13.215.150.32:~/happygear
