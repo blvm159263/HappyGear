c82cef0e5ea10cf5a0f6e227034a64312d78c0d7
nhduchuy <nh.duchuy2306@gmail.com>
2023-04-25 14:54:17 +0700

    update bashscript

--- build-staging-deploy.sh
+++ build-staging-deploy.sh
@@ -1,8 +1,20 @@
+echo "pull all changes from git"
+git pull origin main
 
-# build spring boot application base on staging yml
 echo "Build spring boot application base on staging yml"
 mvn clean package -DskipTests -Pstag
 
-# deploy to staging server
 echo "Deploy to staging server"
 scp -r target happygear@20.2.64.67:~/happygear
+
+echo "go to staging server"
+ssh happygear@20.2.64.67
+
+echo "go to happygear folder"
+cd happygear
+
+echo "build api-server and mysql"
+docker-compose up -d --build api-server mysql
+
+echo "reload nginx"
+docker exec -it happygear_nginx_1 nginx -s reload
